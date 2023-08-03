dccd30429ff482dc78b63b5a05f28762a7c6f7d3
jakrad <127599280+jakrad@users.noreply.github.com>
2023-08-03 17:58:07 +0200

    Added save/load and editing tasks.
    1. Renamed List.cs to TaskListManager.cs for better code organization.
    2. Added saving mechanism using `SaveTasks()` method to store tasks in a text file.
    3. Implemented loading mechanism via `LoadTasks()` method to retrieve tasks from the file on application startup.
    4. Introduced editing functionality with `EditTask()` method to modify existing tasks easily.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿using TaskList;
+﻿using System.Text;
+using TaskList;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        List taskList = new List();
+        Console.OutputEncoding = Encoding.UTF8; // Set console output encoding to UTF-8
+        TaskListManager taskList = new TaskListManager();
 
         Console.WriteLine("Witaj w aplikacji TO DO LIST! \n");
 
+        taskList.LoadTasks(); // Load tasks from the file (if available)
         taskList.ShowMenu();
-
-        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
-        Console.ReadKey();
+        taskList.SaveTasks(); // Save tasks before exiting
     }
 }
--- TaskListManager.cs
+++ TaskListManager.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 
 namespace TaskList
 {
-    internal class List
+    internal class TaskListManager
     {
         private List<string> tasks = new List<string>();
+        private const string FilePath = "tasks.txt"; // Path to the file to store tasks
+
 
         public void ShowMenu()
         {
@@ -20,7 +22,8 @@ namespace TaskList
                 Console.WriteLine("1. Pokaż listę zadań");
                 Console.WriteLine("2. Dodaj nowe zadanie");
                 Console.WriteLine("3. Usuń zadanie");
-                Console.WriteLine("4. Wyjdź z aplikacji");
+                Console.WriteLine("4. Edytuj zadanie");
+                Console.WriteLine("5. Wyjdź z aplikacji");
 
                 // Pobranie wyboru użytkownika
 
@@ -40,6 +43,9 @@ namespace TaskList
                         DeleteTask();
                         break;
                     case "4":
+                        EditTask();
+                        break;
+                    case "5":
                         Console.WriteLine("Dziękuję za skorzystanie z aplikacji TO DO LIST.");
                         return;
                     default:
@@ -83,7 +89,8 @@ namespace TaskList
                 if (taskNumber >=1 && taskNumber <= tasks.Count)
                 {
                     tasks.RemoveAt(taskNumber - 1);
-                    Console.WriteLine("Zadanie usunięto pomyślnie.");
+                    Console.WriteLine("Zadanie usunięto pomyślnie.\n");
+                    ShowTasks() ;
                 }
                 else { Console.WriteLine("Nieprawidłowy numer zadania."); }
             }
@@ -91,6 +98,78 @@ namespace TaskList
             {
                 Console.WriteLine("Nieprawidłowy format numeru zadania.");
             }
+        }
+
+        public void EditTask()
+        {
+            Console.WriteLine("Wybierz, które zadanie chcesz edytować:");
+            ShowTasks();
+            if (tasks.Count == 0) { return; }
+
+            Console.WriteLine("Podaj numer zadania do edycji: ");
+            if (int.TryParse(Console.ReadLine(), out int taskNumber))
+            {
+                if (taskNumber >= 1 && taskNumber <= tasks.Count)
+                {
+                    Console.WriteLine($"Aktualna treść zadania {taskNumber}: {tasks[taskNumber - 1]}");
+                    Console.WriteLine("Podaj nową treść zadania: ");
+                    string editedTask = Console.ReadLine();
+                    tasks[taskNumber - 1] = editedTask;
+                    Console.WriteLine("Zadanie zostało zaktualizowane.");
+                }
+                else
+                {
+                    Console.WriteLine("Nieprawidłowy numer zadania.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowy format numeru zadania.");
+            }
+        }
+
+        public void SaveTasks() 
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(FilePath))
+                {
+                    foreach (string task in tasks)
+                    {
+                        writer.WriteLine(task);
+                    }
+                }
+                Console.WriteLine("Zadania zostały zapisane.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Wystąpił błąd podczas zapisywania zadań: {ex.Message}");
+            }
+        }
+
+        public void LoadTasks()
+        {
+            if (File.Exists(FilePath))
+            {
+                try
+                {
+                    tasks.Clear();
+                    using (StreamReader reader = new StreamReader(FilePath))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            tasks.Add(line);
+                        }
+                    }
+                    Console.WriteLine("Zadania zostały wczytane.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Wystąpił błąd podczas wczytywania zadań: {ex.Message}");
+                }
+            }
+            else { Console.WriteLine("Brak pliku z zadaniami. Nowy plik zostanie utworzony po dodaniu zadania.")  ; }
         }
     }
 }
